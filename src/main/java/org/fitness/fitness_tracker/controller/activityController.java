580bf351726be24e25b239af28fcb51506c18ac7
Harshith N J <njharshith122@gmail.com>
2025-02-09 19:10:04 +0530

    Created the project

---
+++ src/main/java/org/fitness/fitness_tracker/controller/activityController.java
@@ -0,0 +1,5 @@
+package org.fitness.fitness_tracker.controller;
+
+public class activityController {
+    
+}
---
+++ src/main/java/org/fitness/fitness_tracker/dto/activity.java
@@ -0,0 +1,5 @@
+package org.fitness.fitness_tracker.dto;
+
+public class activity {
+    
+}
