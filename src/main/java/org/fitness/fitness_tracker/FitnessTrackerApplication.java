3d36af9be40ebcde81d9dd4d4441cb2825fb06a1
Harshith N J <njharshith122@gmail.com>
2025-02-12 10:28:42 +0530

    Swagger Implemnetation

--- src/main/java/org/fitness/fitness_tracker/FitnessTrackerApplication.java
+++ src/main/java/org/fitness/fitness_tracker/FitnessTrackerApplication.java
@@ -3,7 +3,13 @@ package org.fitness.fitness_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Fitness Tracker API", version = "1.0", description = "Fitness Tracker API",
+					contact = @Contact(name = "Harshith N J", email = "Harshith.Dev2024@outlook.com")))
 public class FitnessTrackerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/fitness/fitness_tracker/controller/activityController.java
+++ src/main/java/org/fitness/fitness_tracker/controller/activityController.java
@@ -11,7 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Activity Controller", description = "Activity Controller API")
 public class activityController {
     
     @Autowired
@@ -19,6 +24,10 @@ public class activityController {
 
     // To Add a activity
     @PostMapping("/activities")
+    @Operation(summary = "Add a activity", description = "Adds a activity to the Database")
+    @ApiResponse(responseCode = "201", description = "Activity Added Successfully")
+    @ApiResponse(responseCode = "400", description = "Activity Already Exists")
+    @ApiResponse(responseCode = "406", description = "Enter the Valid Value for the Calories Burned")
     public ResponseEntity<Object> addActivity(@RequestBody activity activity){
         return service.addActivity(activity);
     }
@@ -30,16 +39,25 @@ public class activityController {
 
     // To Fetch a activity
     @GetMapping("/activities")
+    @Operation(summary = "Get all activities", description = "Retrieves all activities from the Database")
+    @ApiResponse(responseCode = "302", description = "Activities Retrieved Successfully")
+    @ApiResponse(responseCode = "404", description = "Activities Not Found")
     public ResponseEntity<Object> getActivity(){
         return service.getActivity();
     }
 
     @GetMapping("/activities/{name}")
+    @Operation(summary = "Get a activity by name", description = "Retrieves a activity by name from the Database")
+    @ApiResponse(responseCode = "302", description = "Activity Retrieved Successfully")
+    @ApiResponse(responseCode = "404", description = "Activity Not Found")
     public ResponseEntity<Object> getActivityByName(@PathVariable String name){
         return service.getActivityByName(name);
     }
 
     @GetMapping("/activities/date/{date}")
+    @Operation(summary = "Get a activity by date", description = "Retrieves a activity by date from the Database")
+    @ApiResponse(responseCode = "302", description = "Activity Retrieved Successfully")
+    @ApiResponse(responseCode = "404", description = "Activity Not Found")
     public ResponseEntity<Object> getActivityByDate(@PathVariable String date){
         return service.getActivityByDate(date);
     }
@@ -51,10 +69,17 @@ public class activityController {
 
     // To Delete a activity
     @DeleteMapping("/activities/{id}")
+    @Operation(summary = "Delete a activity", description = "Removes a activity from the Database")
+    @ApiResponse(responseCode = "200", description = "Activity Deleted Successfully")
+    @ApiResponse(responseCode = "404", description = "Activity Not Found")
     public ResponseEntity<Object> deleteActivity(@PathVariable Long id){
         return service.deleteActivity(id);
     }
+    
     @DeleteMapping("/activities")
+    @Operation(summary = "Delete all activities", description = "Removes all activities from the Database")
+    @ApiResponse(responseCode = "200", description = "Activities Deleted Successfully")
+    @ApiResponse(responseCode = "404", description = "Activities Not Found")
     public ResponseEntity<Object> deleteAllActivity(){
         return service.deleteAllActivity();
     }
